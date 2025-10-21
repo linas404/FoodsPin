5a37aea2cf9e854e503b1c05b7f9140d01b21ab1
linasdev <info@linasdev.io>
2025-10-21 15:18:37 +0200

    Initial Commit

---
+++ FoodsPin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodsPin
+//
+//  Created by linasdev on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodsPin/FoodsPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodsPinApp.swift
+//  FoodsPin
+//
+//  Created by linasdev on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodsPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
